d86a2b94545412853a213d43f5c28f17f4b2008c
TanziliaM <musatova.tanzilia@gmail.com>
2021-06-23 17:32:07 +0500

    Manager

---
+++ src/main/java/ru/netology/domain/manager/DeleteWallManager.java
@@ -0,0 +1,9 @@
+package ru.netology.domain.manager;
+
+public class DeleteWallManager {
+    private int ownerId;
+    private int postId;
+
+    public void delete(int ownerId, int postId) {
+    }
+}
---
+++ src/main/java/ru/netology/domain/manager/PostManager.java
@@ -0,0 +1,13 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public String generateBlock() {
+        // TODO: add logic
+        return null;
+    }
+}
+
